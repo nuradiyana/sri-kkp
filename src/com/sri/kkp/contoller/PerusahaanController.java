29368cb2f154da5a1984e51fe297b937317244db
Noor Abdullah Adiana <email@nooradiana.com>
2014-05-07 20:57:23 +0700

    Edit some file

---
+++ src/com/sri/kkp/contoller/PerusahaanController.java
@@ -0,0 +1,18 @@
+package com.sri.kkp.contoller;
+
+import com.sri.kkp.view.PanelPerusahaan;
+
+public class PerusahaanController{
+
+    public void cancel(PanelPerusahaan panelPerusahaan) {
+        
+    }
+
+    public void inputPerusahaan(PanelPerusahaan panelPerusahaan) {
+        
+    }
+    
+    private void reset(PanelPerusahaan panelPerusahaan){
+        
+    }    
+}
